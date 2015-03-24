b24ec44a85a1e0bd7cbbc741d868485c4b0bc16c
Brian Sihlongonyane <bryannhlakes@gmail.com>
2015-03-24 16:33:52 +0200

    Deleted some files

--- receipt/views/app.js
+++
@@ -1,7 +0,0 @@
-var http = require ('http');
-
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type' : 'text/plain'});
-	res.end('Hello Mr Brian\n');
-}).listen(8000, '127.0.0.1');
-console.log("This is running on port 8000");
